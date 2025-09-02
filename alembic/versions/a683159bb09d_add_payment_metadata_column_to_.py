00fd4af6cc9a53d845c69446c54108aba6d5acaf
kl63 <kl63@NJIT.edu>
2025-09-02 08:56:02 -0400

    Add payment_metadata column migration for PaymentMethod table

---
+++ alembic/versions/a683159bb09d_add_payment_metadata_column_to_.py
@@ -0,0 +1,24 @@
+"""Add payment_metadata column to PaymentMethod
+
+Revision ID: a683159bb09d
+Revises: 986ffec0cb37
+Create Date: 2025-09-02 08:55:19.559797
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a683159bb09d'
+down_revision = '986ffec0cb37'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('paymentmethod', sa.Column('payment_metadata', sa.JSON(), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('paymentmethod', 'payment_metadata')
